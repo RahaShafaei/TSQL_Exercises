f92dbdc277e93f7d6a3ec12917e9182133c924d1
Raha <44721011+RahaShafaei@users.noreply.github.com>
2022-11-24 17:49:09 +0330

    Update case-study-5.sql

--- 8weeksqlchallenge/case-study-5/case-study-5.sql
+++ 8weeksqlchallenge/case-study-5/case-study-5.sql
@@ -67,11 +67,11 @@ select
   datepart(YEAR, dc.w_date) as [calendar_year],
   [region],
   [platform],
-case
+  case
     when [segment] = 'null' then 'unknown'
     else [segment]
   end [segment],
-case
+  case
     when [segment] = 'null' then 'unknown'
     when CHARINDEX('1', [segment]) > 0 then 'Young Adults'
     when CHARINDEX('2', [segment]) > 0 then 'Middle Aged'
@@ -80,11 +80,13 @@ case
       OR CHARINDEX('4', [segment]) > 0
     ) then 'Retirees'
   end [age_band],
-case
+  case
     when [segment] = 'null' then 'unknown'
     when CHARINDEX('F', [segment]) > 0 then 'Families'
     when CHARINDEX('C', [segment]) > 0 then 'Couples'
   end [demographic],
+  [sales],
+  [transactions],
   cast (([sales] / [transactions]) as decimal(38, 2)) [avg_transaction ] into [data_mart].[dbo].[clean_weekly_sales]
 from
   [data_mart].[dbo].[weekly_sales] ws
@@ -92,22 +94,237 @@ from
   /* ************************************************* 2. Data Exploration*/
   /*========================================================================*/
   -- 1 - What day of the week is used for each week_date value?
+SELECT
+  [week_date],
+  DATEPART(dw, [week_date]) day_num,
+  DATENAME(dw, GETDATE()) day_name,
+  [week_number],
+  [month_number],
+  [calendar_year],
+  [region],
+  [platform],
+  [segment],
+  [age_band],
+  [demographic],
+  [avg_transaction ]
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
   /*========================================================================*/
   -- 2 - What range of week numbers are missing from the dataset?
+select
+  dm_num
+from
+  (
+    values
+      (1),
+      (2),
+      (3),
+      (4),
+      (5),
+      (6),
+      (7)
+  ) as tem(dm_num)
+except
+SELECT
+  DATEPART(dw, [week_date]) day_num
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
+group by
+  [week_date]
   /*========================================================================*/
   -- 3 - How many total transactions were there for each year in the dataset?
+SELECT
+  DATEPART(year, [week_date]) d_year,
+  sum([transactions]) tot_trans
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
+group by
+  DATEPART(year, [week_date])
   /*========================================================================*/
   -- 4 - What is the total sales for each region for each month?
+SELECT
+  [region],
+  DATEPART(MONTH, [week_date]) mnt,
+  SUM(sales) tot_trans
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
+group by
+  [region],
+  DATEPART(MONTH, [week_date])
+order by
+  [region],
+  DATEPART(MONTH, [week_date])
   /*======================================================================== */
   -- 5 - What is the total count of transactions for each platform
+SELECT
+  [platform],
+  sum([transactions]) tot_trans
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
+group by
+  [platform]
   /*======================================================================== */
   -- 6 - What is the percentage of sales for Retail vs Shopify for each month?
+;
+
+with sm_sale as(
+  SELECT
+    DATEPART(MONTH, [week_date]) mnt,
+    [platform],
+    SUM(cast(sales as decimal(38, 2))) sum_sale
+  FROM
+    [data_mart].[dbo].[clean_weekly_sales]
+  group by
+    DATEPART(MONTH, [week_date]),
+    [platform]
+)
+select
+  mnt,
+  [platform],
+  sum_sale,
+  cast(
+    (
+      cast (
+        case
+          when [platform] = 'Retail' then sum_sale
+          else NULL
+        end as decimal
+      ) / (
+        SUM(cast(sum_sale as decimal(38, 2))) over (partition by [platform])
+      )
+    ) * 100 as numeric(38, 1)
+  ) prc_Retail,
+  cast(
+    (
+      cast (
+        case
+          when [platform] = 'Shopify' then sum_sale
+          else NULL
+        end as decimal
+      ) / (
+        SUM(cast(sum_sale as decimal(38, 2))) over (partition by [platform])
+      )
+    ) * 100 as numeric(38, 1)
+  ) prc_Shopify
+from
+  sm_sale
+order by
+  mnt,
+  [platform]
   /*======================================================================== */
   -- 7 - What is the percentage of sales by demographic for each year in the dataset?
+;
+
+with sm_sale as(
+  SELECT
+    DATEPART(YEAR, [week_date]) yr,
+    [demographic],
+    SUM(cast(sales as decimal(38, 2))) sum_sale
+  FROM
+    [data_mart].[dbo].[clean_weekly_sales]
+  group by
+    DATEPART(YEAR, [week_date]),
+    [demographic]
+)
+select
+  yr,
+  [demographic],
+  sum_sale,
+  cast(
+    (
+      cast (
+        case
+          when [demographic] = 'Couples' then sum_sale
+          else NULL
+        end as decimal
+      ) / (
+        SUM(cast(sum_sale as decimal(38, 2))) over (partition by [demographic])
+      )
+    ) * 100 as numeric(38, 1)
+  ) prc_Couples,
+  cast(
+    (
+      cast (
+        case
+          when [demographic] = 'Families' then sum_sale
+          else NULL
+        end as decimal
+      ) / (
+        SUM(cast(sum_sale as decimal(38, 2))) over (partition by [demographic])
+      )
+    ) * 100 as numeric(38, 1)
+  ) prc_Families,
+  cast(
+    (
+      cast (
+        case
+          when [demographic] = 'unknown' then sum_sale
+          else NULL
+        end as decimal
+      ) / (
+        SUM(cast(sum_sale as decimal(38, 2))) over (partition by [demographic])
+      )
+    ) * 100 as numeric(38, 1)
+  ) prc_unknown
+from
+  sm_sale
+order by
+  yr,
+  [demographic]
   /*======================================================================== */
   -- 8 - Which age_band and demographic values contribute the most to Retail sales?
+  /*  Which age_band and demographic values contribute the most to Retail sales? */
+;
+
+with dem_max as (
+  SELECT
+    top 1 'demographic' [type],
+    [demographic],
+    SUM(cast(sales as decimal(38, 2))) sum_sale
+  FROM
+    [data_mart].[dbo].[clean_weekly_sales]
+  where
+    [demographic] != 'unknown'
+  group by
+    [demographic]
+  order by
+    SUM(cast(sales as decimal(38, 2))) DESC
+),
+age_max as(
+  SELECT
+    top 1 'age_band' [type],
+    [age_band],
+    SUM(cast(sales as decimal(38, 2))) sum_sale
+  FROM
+    [data_mart].[dbo].[clean_weekly_sales]
+  where
+    [age_band] != 'unknown'
+  group by
+    [age_band]
+  order by
+    SUM(cast(sales as decimal(38, 2))) DESC
+)
+select
+  *
+from
+  dem_max
+union
+select
+  *
+from
+  age_max
   /*======================================================================== */
   -- 9 - Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+  /*Answer: This file cannot be considered as an average, because it is calculating the amount of sales based on the number of transactions, and it is not related to the average transaction at all, and of course it is based on toltal of dataset and not by year.*/
+SELECT
+  DATEPART(YEAR, [week_date]) yr,
+  [platform],
+  AVG(cast([transactions] as decimal(38, 2))) trans_Retail
+FROM
+  [data_mart].[dbo].[clean_weekly_sales]
+group by
+  DATEPART(YEAR, [week_date]),
+  [platform]
   /* ************************************************* 3. Before & After Analysis*/
   /* This technique is usually used when we inspect an important event and want to inspect the impact before and after a certain point in time.
    Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging changes came into effect.
