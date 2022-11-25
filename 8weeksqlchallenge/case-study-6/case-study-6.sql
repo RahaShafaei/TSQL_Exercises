54d4493fc947aa100c155795cac7bf62d998f78d
Raha <44721011+RahaShafaei@users.noreply.github.com>
2022-11-25 15:50:12 +0330

    Update case-study-6.sql

--- 8weeksqlchallenge/case-study-6/case-study-6.sql
+++ 8weeksqlchallenge/case-study-6/case-study-6.sql
@@ -4,56 +4,160 @@
 /* ************************************************* 2. Digital Analysis*/
 /*========================================================================*/
 -- 1 - How many users are there?
-/*========================================================================*/
--- 2 - How many cookies does each user have on average?
-/*========================================================================*/
--- 3 - What is the unique number of visits by all users per month?
-/*========================================================================*/
--- 4 - What is the number of events for each event type?
-/*======================================================================== */
--- 5 - What is the percentage of visits which have a purchase event?
-/*======================================================================== */
--- 6 - What is the percentage of visits which view the checkout page but do not have a purchase event?
-/*======================================================================== */
--- 7 - What are the top 3 pages by number of views?
-/*======================================================================== */
--- 8 - What is the number of views and cart adds for each product category?
-/*======================================================================== */
--- 9 - What are the top 3 products by purchases?
-/*======================================================================== */
-/* ************************************************* 3. Product Funnel Analysis*/
-/* Using a single SQL query - create a new output table which has the following details:
- How many times was each product viewed?
- How many times was each product added to cart?
- How many times was each product added to a cart but not purchased (abandoned)?
- How many times was each product purchased?
- Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.*/
-/*======================================================================== */
--- 1 - Which product had the most views, cart adds and purchases?
-/*======================================================================== */
--- 2 - Which product was most likely to be abandoned?
-/*======================================================================== */
--- 3 - Which product had the highest view to purchase percentage?
-/*======================================================================== */
--- 4 - What is the average conversion rate from view to cart add?
-/*======================================================================== */
--- 5 - What is the average conversion rate from cart add to purchase?
-/* ************************************************* 3. Campaigns Analysis*/
-/* Generate a table that has 1 single row for every unique visit_id record and has the following columns:
- user_id
- visit_id
- visit_start_time: the earliest event_time for each visit
- page_views: count of page views for each visit
- cart_adds: count of product cart add events for each visit
- purchase: 1/0 flag if a purchase event exists for each visit
- campaign_name: map the visit to a campaign if the visit_start_time falls between the start_date and end_date
- impression: count of ad impressions for each visit
- click: count of ad clicks for each visit
- (Optional column) cart_products: a comma separated text value with products added to the cart sorted by the order they were added to the cart (hint: use the sequence_number)
- Use the subsequent dataset to generate at least 5 insights for the Clique Bait team - bonus: prepare a single A4 infographic that the team can use for their management reporting sessions, be sure to emphasise the most important points from your findings.
- Some ideas you might want to investigate further include:
- Identifying users who have received impressions during each campaign period and comparing each metric with other users who did not have an impression event
- Does clicking on an impression lead to higher purchase rates?
- What is the uplift in purchase rate when comparing users who click on a campaign impression versus users who do not receive an impression? What if we compare them with users who just an impression but do not click?
- What metrics can you use to quantify the success or failure of each campaign compared to eachother?
- */
+SELECT
+  count(distinct [user_id]) cnt_usr
+FROM
+  [clique_bait].[dbo].[users]
+  /*========================================================================*/
+  -- 2 - How many cookies does each user have on average?
+;
+
+with cnt_cookies as(
+  SELECT
+    [user_id],
+    count(*) cnt_cok
+  FROM
+    [clique_bait].[dbo].[users]
+  group by
+    [user_id]
+)
+select
+  avg(cnt_cok) avg_cook
+from
+  cnt_cookies
+  /*========================================================================*/
+  -- 3 - What is the unique number of visits by all users per month?
+SELECT
+  datepart(month, [event_time]) mnt,
+  count(distinct [visit_id]) cnt_vst
+FROM
+  [clique_bait].[dbo].[events]
+group by
+  datepart(month, [event_time])
+order by
+  datepart(month, [event_time])
+  /*========================================================================*/
+  -- 4 - What is the number of events for each event type?
+SELECT
+  ei.event_name,
+  count(*) cnt_evn
+FROM
+  [clique_bait].[dbo].[events] e
+  join [clique_bait].[dbo].event_identifier ei on e.event_type = ei.event_type
+group by
+  ei.event_name,
+  ei.event_type
+order by
+  ei.event_type
+  /*======================================================================== */
+  -- 5 - What is the percentage of visits which have a purchase event?
+SELECT
+  (
+    cast(
+      count(
+        case
+          when ei.event_name = 'Purchase' then ei.event_name
+          else NULL
+        end
+      ) as decimal(38, 2)
+    ) / cast(count(*) as decimal(38, 2))
+  ) * 100 cnt_purch
+FROM
+  [clique_bait].[dbo].[events] e
+  join [clique_bait].[dbo].event_identifier ei on e.event_type = ei.event_type
+  /*======================================================================== */
+  -- 6 - What is the percentage of visits which view the checkout page but do not have a purchase event?
+;
+
+with calc_cnts as (
+  SELECT
+    count(distinct e.visit_id) cnt_vst,
+    case
+      when count(
+        case
+          when ph.page_name = 'Checkout' then ph.page_name
+          else NULL
+        end
+      ) > 0 then 1
+      else 0
+    end cnt_check,
+    count(
+      case
+        when ei.event_name = 'Purchase' then ph.page_name
+        else NULL
+      end
+    ) cnt_purch
+  FROM
+    [clique_bait].[dbo].[events] e
+    join [clique_bait].[dbo].event_identifier ei on e.event_type = ei.event_type
+    join [clique_bait].[dbo].page_hierarchy ph on e.page_id = ph.page_id
+  group by
+    e.visit_id
+)
+select
+  (
+    cast(
+      sum(
+        case
+          when cnt_check > 0
+          and cnt_purch = 0 then cnt_check
+          else 0
+        end
+      ) as decimal(38, 2)
+    ) / cast (sum(cnt_vst) as decimal(38, 2))
+  ) * 100 as avg_chk
+from
+  calc_cnts
+  /*======================================================================== */
+  -- 7 - What are the top 3 pages by number of views?
+SELECT
+  top 3 page_name,
+  count(*) cnt_page
+FROM
+  [clique_bait].[dbo].[events] e
+  join [clique_bait].[dbo].[page_hierarchy] ph on ph.page_id = e.page_id
+group by
+  page_name
+order by
+  count(*) desc
+  /*======================================================================== */
+  -- 8 - What is the number of views and cart adds for each product category?
+  /*======================================================================== */
+  -- 9 - What are the top 3 products by purchases?
+  /*======================================================================== */
+  /* ************************************************* 3. Product Funnel Analysis*/
+  /* Using a single SQL query - create a new output table which has the following details:
+   How many times was each product viewed?
+   How many times was each product added to cart?
+   How many times was each product added to a cart but not purchased (abandoned)?
+   How many times was each product purchased?
+   Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.*/
+  /*======================================================================== */
+  -- 1 - Which product had the most views, cart adds and purchases?
+  /*======================================================================== */
+  -- 2 - Which product was most likely to be abandoned?
+  /*======================================================================== */
+  -- 3 - Which product had the highest view to purchase percentage?
+  /*======================================================================== */
+  -- 4 - What is the average conversion rate from view to cart add?
+  /*======================================================================== */
+  -- 5 - What is the average conversion rate from cart add to purchase?
+  /* ************************************************* 3. Campaigns Analysis*/
+  /* Generate a table that has 1 single row for every unique visit_id record and has the following columns:
+   user_id
+   visit_id
+   visit_start_time: the earliest event_time for each visit
+   page_views: count of page views for each visit
+   cart_adds: count of product cart add events for each visit
+   purchase: 1/0 flag if a purchase event exists for each visit
+   campaign_name: map the visit to a campaign if the visit_start_time falls between the start_date and end_date
+   impression: count of ad impressions for each visit
+   click: count of ad clicks for each visit
+   (Optional column) cart_products: a comma separated text value with products added to the cart sorted by the order they were added to the cart (hint: use the sequence_number)
+   Use the subsequent dataset to generate at least 5 insights for the Clique Bait team - bonus: prepare a single A4 infographic that the team can use for their management reporting sessions, be sure to emphasise the most important points from your findings.
+   Some ideas you might want to investigate further include:
+   Identifying users who have received impressions during each campaign period and comparing each metric with other users who did not have an impression event
+   Does clicking on an impression lead to higher purchase rates?
+   What is the uplift in purchase rate when comparing users who click on a campaign impression versus users who do not receive an impression? What if we compare them with users who just an impression but do not click?
+   What metrics can you use to quantify the success or failure of each campaign compared to eachother?
+   */
